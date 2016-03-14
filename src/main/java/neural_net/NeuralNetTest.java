f33041cf05e4765c7bcd20746985f7b1296a4637
luisfisherjr <luisfisherjr@outlook.com>
2016-03-14 01:42:08 -0700

    added matrix lib
    - added apache commons v 3.6 to dependency to pom.xml
    - played with matrix lib some

--- src/main/java/neural_net/NeuralNetTest.java
+++ src/main/java/neural_net/NeuralNetTest.java
@@ -1,15 +1,25 @@
 package neural_net;
+import org.apache.commons.math3.linear.BlockRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
 
 public class NeuralNetTest {
 
 	public static void main(String[] args) {
+		int m = 3;
+		int n = 3;
 		
-		System.out.println("Hello World!");
-		System.out.println("lol");
-		System.out.println("lol");
-		NeuralNet n = new NeuralNet();
-		n.initNet();
-		n.printNet();
+		// testing out matrix class
+		double[][] data = {{1,2,3},{4,5,6},{7,8,9}};
+		
+		RealMatrix test1 = new BlockRealMatrix(data);
+		System.out.println(test1.getEntry(1, 1));
+		
+		RealMatrix test2 = new BlockRealMatrix(m,n);
+		System.out.println(test2.getEntry(1, 1));
+
+		NeuralNet nn = new NeuralNet();
+		nn.initNet();
+		nn.printNet();
 	}
 	
 }
